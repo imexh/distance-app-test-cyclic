e0c7c2bb5f30376cba519d99bcad36b3c3bd5d4d
imexh <chandikaranawella@gmail.com>
2023-12-14 22:12:46 +0530

    Distance calculator print on console

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const video = document.getElementById('video')
+// var distanceText = document.getElementById('distance')
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -28,6 +29,28 @@ video.addEventListener('play', () => {
             .withFaceExpressions()
             const resizedDetections = faceapi.resizeResults(detections, displaySize)
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
+            if (resizedDetections.length > 0) {
+                // Get coordinations of left and right eyes
+                const landmarks = resizedDetections[0].landmarks;
+                const leftEye = landmarks.getLeftEye();
+                const rightEye = landmarks.getRightEye();
+                
+                // Calculate the distance between the eyes in units pixels
+                const distanceBetweenEyes = Math.sqrt(
+                    Math.pow(leftEye[0]._x - rightEye[3]._x, 2) +
+                    Math.pow(leftEye[0]._y - rightEye[3]._y, 2)
+                );
+
+                const averageHumanFaceWidth = 6.3; // In centimeters (Need to be accurate)
+                const focalLength = 610; // Estimated focal length of the camera (Changes with camera)
+
+                const distance = (averageHumanFaceWidth * focalLength) / distanceBetweenEyes + 30 // Correct the equation
+    
+                console.log(`Width of Eyes: ${distanceBetweenEyes}px, Distance: ${distance}cm`);
+                // distanceText.textContent = distance + "cm"
+            }
+
             faceapi.draw.drawDetections(canvas, resizedDetections)
             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
             faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
